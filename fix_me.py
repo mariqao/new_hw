54948c82cfed34206754b8c9b53ddc6e99ed2ab1
Marina <marinesiko@mail.ru>
2024-03-01 18:16:48 +0300

    Add files via upload

--- fix_me.py
+++ fix_me.py
@@ -1,6 +1,5 @@
 def calculate_average(nums):
     total = sum(nums)
-<<<<<<< HEAD
     count = len(nums)
     average = total / count
     return int(average)
@@ -9,11 +8,3 @@ def calculate_average(nums):
 nums = [10, 15, 20]
 result = calculate_average(nums)
 print("The average is:", result)
-=======
-    count = len(numbers)
-    average = total / count
-
-nums = [10, 15, 20]
-result = calculat_average(nums)
-Print("The average is:", result)
->>>>>>> origin/pr1
